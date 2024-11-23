9ee46b5f241acc5af5c71f7a18df160952917de4
pherwaniharsh <rmpherwani.hp@protonmail.com>
2024-11-23 20:57:20 +0530

    Bug Fixes

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,19 @@
-// let switchItUp = document.querySelector(".fa-regular");
-// let resume = document.querySelector(".resume");
+let switchItUp = document.querySelector(".fa-regular");
+let resume = document.querySelector(".resume");
 
-// switchItUp.onclick = function(){
-//     if(switchItUp.classList.contains("fa-sun")){
+switchItUp.onclick = function(){
+    if(switchItUp.classList.contains("fa-sun")){
         
-//         switchItUp.classList.remove("fa-sun");
-//         switchItUp.classList.add("fa-moon");
-//         resume.classList.add("darkMode");
-//     }
-//     else{
-//         switchItUp.classList.remove("fa-moon");
-//         switchItUp.classList.add("fa-sun");
-//         resume.classList.remove("darkMode");
-//     }
-// };
+        switchItUp.classList.remove("fa-sun");
+        switchItUp.classList.add("fa-moon");
+        resume.classList.add("darkMode");
+    }
+    else{
+        switchItUp.classList.remove("fa-moon");
+        switchItUp.classList.add("fa-sun");
+        resume.classList.remove("darkMode");
+    }
+};
 
 let hamburger = document.querySelector(".fa-bars");
 let secondMenu = document.querySelector(".secondMenu");
